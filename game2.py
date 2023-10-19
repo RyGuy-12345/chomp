d3fa22e7c3dcbe388cf139e964986a9307dd00a2
RyGuy-12345 <m262016@usna.edu>
2023-10-19 10:20:27 -0400

    game2 added

--- game2.py
+++ game2.py
@@ -8,21 +8,18 @@ screen_width = 800
 screen_height = 600
 tile_size = 64
 
-BLUE = (0, 0, 225)
-Brown = (222, 204, 113)
-
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Ocean")
 
-def draw_background(screen):
-    water = pygame.image.load("../labs/L7/chomp/sprites/water.png").convert()
-    sand = pygame.image.load("../labs/L7/chomp/sprites/sand_top.png").convert()
-    seagrass = pygame.image.load("../labs/L7/chomp/sprites/seagrass.png").convert()
+custom_font = pygame.font.Font("../labs/L8/fonts/Brainfish_Rush.ttf")
 
+def draw_background(screen):
+    water = pygame.image.load("../labs/L7/sprites/water.png").convert()
+    sand = pygame.image.load("../labs/L7/sprites/sand_top.png").convert()
+    seagrass = pygame.image.load("../labs/L7/sprites/seagrass.png").convert()
     sand.set_colorkey((0, 0, 0))
     seagrass.set_colorkey((0, 0, 0))
 
-   #water
     for x in range(0,screen_width,tile_size):
         for y in range(0,screen_height, tile_size):
             screen.blit(water, (x,y))
@@ -35,6 +32,10 @@ def draw_background(screen):
     for _ in range(7):
         x = random.randint(0,screen_width)
         screen.blit(seagrass, (x, screen_height- tile_size*2+5))
+
+
+    text = custom_font.render("chomp", True, (255, 29, 0))
+    screen.blit(text, (screen_width/2-text.get_width()/2, screen_height/2-text.get_height()/2))
 
 
 running = True
