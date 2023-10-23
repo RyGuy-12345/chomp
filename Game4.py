0cfbaab45855c236ddb292c80ad72916b2779b2a
RyGuy-12345 <m262016@usna.edu>
2023-10-23 11:49:13 -0400

    added game4 and code to fish

--- Game4.py
+++ Game4.py
@@ -9,10 +9,12 @@ screen_width = 800
 screen_height = 600
 tile_size = 64
 
+#add pygame clock
+clock = pygame.time.Clock()
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Ocean")
 
-custom_font = pygame.font.Font("fonts/Black_Crayon.ttf", 115)
 
 def draw_background(screen):
     water = pygame.image.load("sprites/water.png").convert()
@@ -34,9 +36,9 @@ def draw_background(screen):
         x = random.randint(0,screen_width)
         screen.blit(seagrass, (x, screen_height- tile_size*2+5))
 
-
+    custom_font = pygame.font.Font("fonts/Black_Crayon.ttf", 115)
     text = custom_font.render("chomp", True, (255, 29, 0))
-    screen.blit(text, (screen_width/2-text.get_width()/2, screen_height/7-text.get_height()/2))
+    screen.blit(text, (screen_width / 2 - text.get_width() / 2, screen_height / 7 - text.get_height() / 2))
 
 
 #main loop
@@ -44,9 +46,8 @@ running = True
 background = screen.copy()
 draw_background(background)
 
-
 for a in range(5):
-    fishes.add(Fish(random.randint(0, screen_width - tile_size), random.randint(0, screen_height - tile_size*2)))
+    fishes.add(Fish(random.randint(screen_width, screen_width*2), random.randint(tile_size, screen_height - tile_size*2)))
 
 
 while running:
@@ -56,7 +57,18 @@ while running:
 
     screen.blit(background, (0,0))
 
-    fishes.draw(background)
+    #update fish position
+    fishes.update()
+
+    #check if fish have left the screen
+    for fish in fishes:
+        if fish.rect.x < -fish.rect.width:
+            fishes.remove(fish)
+            fishes.add(Fish(random.randint(screen_width, screen_width * 2), random.randint(tile_size, screen_height - tile_size * 2)))
+
+    fishes.draw(screen)
     pygame.display.flip()
 
+    #set framerate
+    clock.tick(100)
 pygame.quit()
--- fish.py
+++ fish.py
@@ -1,19 +1,31 @@
 #create a pygame sprite class for a fish
+import random
 
 import pygame
+
+MIN_SPEED = 1.5
+MAX_SPEED = 3.5
 
 class Fish(pygame.sprite.Sprite):
     def __init__(self, x,y):
         super().__init__()
         self.image = pygame.image.load("sprites/green_fish.png").convert()
         self.image.set_colorkey((0,0,0))
-        pygame.transform.flip(self.image, True, False)
+        self.image = pygame.transform.flip(self.image, True, False)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        self.speed = random.uniform(MIN_SPEED, MAX_SPEED)
         self.rect.center = (x,y)
+
+    def update(self):
+        self.x -= self.speed
+        self.rect.x = self.x
+
+
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+        pygame.transform.flip()
 
 fishes = pygame.sprite.Group()
